d4b5f5a98233993edc5734b6aa6ab8636267de1c
Allan Binayao <allan.binayao@gmail.com>
2020-05-24 12:11:50 -0500

    pushing desktop work

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Container from './components/Container/index'
-import Row from './components/Row/index'
-import Col from './components/Col/index'
 import Employees from './components/Employees/index'
 
 function App() {
   return (
     <div>
       <Container>
-        <Row>
          <Employees>
-           
          </Employees>
-        </Row>
       </Container>
     </div>
   )
--- employee-directory/src/components/Employees/index.js
+++ employee-directory/src/components/Employees/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import API from '../../utils/API'
+import EmployeeTableRow from "../EmployeeTableRow/index"
 
 function Employees() {
     const [users, setUsers] = useState([])
     const [error, setError] = useState("")
+    const [search, setSearch] = useState()
 
     useEffect(() => {
         API.getUsers()
@@ -15,34 +17,32 @@ function Employees() {
             .catch(err => setError(err))
     }, []);
 
-    console.log(users)
-    const userList = users.map(user =>
-        <tr key={user.login.uuid}>
-            <td>
-                <img src={user.picture.large} alt={user.login.username}></img>
-            </td>
-            <td>
-                <span>{user.name.first} {user.name.last}</span>
-            </td>
-            <td>
-                <span>{user.login.username}</span>
-            </td>
-        </tr>
-    )
+    // useEffect(() => {
+    //     setUsers(users.filter(user => {
+    //         const name = user.name.last
+    //         return (users.map(() => name.indexOf(search) !== -1))
+    //     }
+    //     ))
+    // },[search, users])
 
-    return (
-        <div>
-            <table>
-                <tr>
-                    <th>Picture
-                    </th>
-                    <th>Name</th>
-                    <th>Username</th>
-                </tr>
-                {userList}
-            </table>
-        </div>
+    console.log(users)
+    const userList = users.map(user => 
+        <EmployeeTableRow users={users}>
+        </EmployeeTableRow>
     )
+        // <tr key={user.login.uuid}>
+        //     <td>
+        //         <img src={user.picture.large} alt={user.login.username}></img>
+        //     </td>
+        //     <td>
+        //         <span>{user.name.first} {user.name.last}</span>
+        //     </td>
+        //     <td>
+        //         <span>{user.login.username}</span>
+        //     </td>
+        // </tr>
+
+    return ({userList})
 }
 
 export default Employees;
